2be27a63669f595b272016e6f5d1181246182f37
yas43 <ykeshtdar@yahoo.com>
2024-11-15 16:01:47 +0100

    initial comment

---
+++ src/main/java/com/example/simpleAuthentication_JWT_base_/SimpleAuthenticationJwtBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleAuthentication_JWT_base_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthenticationJwtBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAuthenticationJwtBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleAuthentication_JWT_base_/SimpleAuthenticationJwtBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleAuthentication_JWT_base_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuthenticationJwtBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
