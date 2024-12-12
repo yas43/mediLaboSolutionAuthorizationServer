a331199e72e88b819310613dfcc9fb25eb00f47f
yas43 <ykeshtdar@yahoo.com>
2024-12-13 00:53:48 +0100

    security adaptation on patient info token tracked and inject by session id in front prefilter token transfer by header from front controller to services api

--- src/main/java/com/example/simpleAuthentication_JWT_base_/service/JWTService.java
+++ src/main/java/com/example/simpleAuthentication_JWT_base_/service/JWTService.java
@@ -15,7 +15,7 @@ import java.util.function.*;
 public class JWTService {
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
     private static final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
-    private final long validityInMilliseconds = 1000*60; // 1 hour
+    private final long validityInMilliseconds = 1000*60*60; // 1 hour
 
 
 
@@ -28,25 +28,54 @@ public class JWTService {
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(now)
                 .setExpiration(expiry)
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .signWith(key,SignatureAlgorithm.HS256)
                 .compact();
     }
 
 
 
     public boolean validateToken(String token) {
+
         try {
-            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            // Use the parserBuilder to build a parser and validate the token
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
             return true;
         } catch (JwtException | IllegalArgumentException e) {
+            // Handle invalid token
             return false;
         }
+//    }
+
+//        try {
+//            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+//            return true;
+//        } catch (JwtException | IllegalArgumentException e) {
+//            return false;
+//        }
     }
 
 
 
     public String getUsername(String token) {
-        return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody().getSubject();
+        System.out.println("inside jwtservice in authorization server token is "+ token);
+
+
+        // Create a signing key from the secret
+        Key signingKey = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+        // Parse the JWT and get the subject (username)
+        String username = Jwts.parserBuilder()
+                .setSigningKey(signingKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+        System.out.println("inside jwtservice in authorization server username is "+username);
+        return username;
+//        return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody().getSubject();
     }
 
 
